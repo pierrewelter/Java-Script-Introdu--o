225c2808277c9d1660fd6173c759cafd6a606ab6
pierrewelter <pierre_welter@estudante.sesisenai.org.br>
2025-03-19 09:42:21 -0300

    ADD - par ou impar

---
+++ par.js
@@ -0,0 +1,17 @@
+console.log("hello World")
+function VerificarPar(numero){    
+    
+    if(numero % 2 === 0){
+    // === é estritamente igual, é igual em tipo e em conteudo
+    console.log('par')
+}else{
+    console.log('impar')
+
+}
+
+
+}
+
+
+VerificarPar(4);
+VerificarPar(7);
